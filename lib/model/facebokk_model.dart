704c757dfce32793728109ebed52d5ed69ad35a0
Joseph1997-bit <83473615+Joseph1997-bit@users.noreply.github.com>
2022-12-20 13:51:49 +0300

    Update facebokk_model.dart

--- lib/model/facebokk_model.dart
+++ lib/model/facebokk_model.dart
@@ -1,19 +1,4 @@
-/*
-retrive the user information using the GraphAPI
-fields string of fields like birthday,email,hometown
-The facebook SDK will return a JSON like
-{
-"name": "Open Graph Test User",
-"email": "open_jygexjs_user@tfbnw.net",
-"picture": {
-"data": {
-"height": 126,
-"url": "https://scontent.fuio21-1.fna.fbcdn.net/v/t1.30497-1/s200x200/84628273_176159830277856_972693363922829312_n.jpg",
-"width": 200
-}
-},
-"id": "136742241592917"
-}*/
+
 
 //FaceBook girisi sagladiktan sonra alacgimiz bilgi json olark ve hepsi Strin tipi oYuzden degiskenleri String olark tanimladik
 import 'dart:convert';
